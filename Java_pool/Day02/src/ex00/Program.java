0bec0419dc7b1faec4ff3b43f16106275163a901
SinchDmitry <sinchdmitry@gmail.com>
2022-07-15 22:48:31 +0300

    -J03-15072022-

--- Java_pool/Day02/src/ex00/Program.java
+++ Java_pool/Day02/src/ex00/Program.java
@@ -1,12 +1,25 @@
 package ex00;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
         SignatureChecker a = new SignatureChecker();
         if (a.createMap()) {
-            a.findSign("/Users/aarchiba/Desktop/Educational_projects_on_Cpp_and_Java/Java_pool/Day02/en.subject.pdf");
-            a.findSign("/Users/aarchiba/Desktop/Educational_projects_on_Cpp_and_Java/Java_pool/Day02/src/ex00/Program.java");
-            a.findSign("/Users/aarchiba/Desktop/Educational_projects_on_Cpp_and_Java/Java_pool/Day02/src/ex00/png.png");
+            while (true) {
+                Scanner input = new Scanner(System.in);
+                if (input.hasNextLine()) {
+                    String path = input.nextLine();
+                    if (path.equals("42")){
+                        break;
+                    } else {
+                        a.findSign(path);
+                    }
+                }
+            }
+//            a.findSign("/Users/aarchiba/Desktop/Educational_projects_on_Cpp_and_Java/Java_pool/Day02/en.subject.pdf");
+//            a.findSign("/Users/aarchiba/Desktop/Educational_projects_on_Cpp_and_Java/Java_pool/Day02/src/ex00/Program.java");
+//            a.findSign("/Users/aarchiba/Desktop/Educational_projects_on_Cpp_and_Java/Java_pool/Day02/src/ex00/png.png");
         } else {
             System.err.println("Error : empty signature");
             System.exit(-1);
---
+++ Java_pool/Day03/src/ex03/FileLauncher.java
@@ -0,0 +1,108 @@
+package ex03;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class FileLauncher extends Thread {
+    private int                     _numThreads;
+    private List<FileThread>        _workList;
+    private BlockingQueue<FileInfo> _queue;
+
+    private static class FileInfo {
+        private final String _url;
+        private final int _numOfFile;
+
+        public FileInfo(String url, int num) {
+            _url = url;
+            _numOfFile = num;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%d - %s\n", _numOfFile, _url);
+        }
+    }
+
+    private static class FileThread extends Thread {
+        private final BlockingQueue<FileInfo> _workQueue;
+
+        public FileThread(BlockingQueue<FileInfo> queue) {
+            _workQueue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (this._workQueue) {
+                    if (this._workQueue.isEmpty()) {
+                        return;
+                    }
+                }
+                FileInfo info = null;
+                try {
+                    info = this._workQueue.take();
+                    System.out.printf("%s start download file number %d\n", this.getName(), info._numOfFile);
+                    String fileName = info._url.substring(info._url.lastIndexOf('/') + 1);
+                    URL dlUrl = new URL(info._url);
+                    InputStream urlStream = dlUrl.openStream();
+                    Files.copy(urlStream, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+                    System.out.printf("%s finish download file number %d\n", this.getName(), info._numOfFile);
+                } catch (InterruptedException e) {
+                    System.out.printf("%s caught InterruptedException\n", this.getName());
+                } catch (MalformedURLException e) {
+                    System.err.printf("Error : %s file %d URL is malformed\n", this.getName(), info._numOfFile);
+                } catch (IOException e) {
+                    System.err.printf("Error : %s download file %d failed: %s\n", this.getName(), info._numOfFile, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public FileLauncher(int numOfThread, Map<Integer, String> fileData) {
+        if (numOfThread > 0) {
+            _numThreads = numOfThread;
+            _queue = new LinkedBlockingQueue<>();
+            _workList = new ArrayList<>();
+//            System.out.println(fileData.size());
+            for (int i = 1; i <= fileData.size(); ++i) {
+                try {
+                    FileInfo tmpData = new FileInfo(fileData.get(i), i);
+                    _queue.put(tmpData);
+//                    System.out.println(tmpData);
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+            for (int i = 0; i < _numThreads; ++i) {
+                FileThread tmp = new FileThread(_queue);
+                _workList.add(tmp);
+            }
+            System.out.println(_workList);
+        } else {
+            System.out.println("Error : wrong number of threads");
+        }
+    }
+
+    public void generalAction() {
+        for (FileThread i : _workList ) {
+            i.start();
+        }
+        for (FileThread i : _workList ) {
+            try {
+                i.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
